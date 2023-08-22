35a648a34e253923154ad18fe013b1044824f52b
fborlas-adish <francis.borlas@adish.com.ph>
2023-08-22 15:48:48 +0800

    add rolify gem, UI updates, action validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ApplicationRecord
+  resourcify
+
   has_many_attached :media
   belongs_to :user
 
   validates :body, presence: true, length: { minimum: 1 }
+
+  def can_update?(updater)
+    user == updater
+  end
 end
